2b0bb4caf55381a18f7e88565036eae56fc69bf0
Pablo-Penasco <pccraig@icloud.com>
2023-01-09 18:55:57 -0500

    add random number generator to replace user provided number.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,29 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Please enter a random number: ");
-        string userRandomNumber = Console.ReadLine();
-        int x = int.Parse(userRandomNumber);
-
+        // Generate a random number.
+        Random randomGenerator = new Random();
+        int x = randomGenerator.Next(1, 100);
         
-        
-
+        //Declare a variable for the user input.
         int guessNumber = 0;        
 
-
+        //Take user input and compare to random number {x} until user input matches the random number.
         while (guessNumber != x)
         {
-            Console.Write("Guess the random number: ");
+            Console.Write("What is your guess? ");
             string userInputGuess = Console.ReadLine();
             guessNumber = int.Parse(userInputGuess);
             
                 if (guessNumber < x)
             {
-                Console.WriteLine("Less");
+                Console.WriteLine("Higher");
             }
             else if (guessNumber > x)
             {
-                Console.WriteLine("Greater");
+                Console.WriteLine("Lower");
             }
             else
             {
