95f00bfc6a96fec641c4be6b5a1d319c65ee67d7
Pablo-Penasco <pccraig@icloud.com>
2023-01-10 19:53:10 -0500

    created list and let user keep adding numbers to it until they enter 0

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Declare variable for user input.
+        string userInput = "";
+
+        //Create a list of intergers.
+        List<int> numbers = new List<int>();
+
+        //Ask for user to input a number. Keep adding it to the "numbers" list until they enter 0.
+        while (userInput != "0")
+        {
+            
+            Console.Write("Please add a number to the list. Enter 0 when you are done: ");
+            userInput = Console.ReadLine();
+            int userNumber = int.Parse(userInput);
+
+            numbers.Add(userNumber);
+        }
+        
+        
+
+        
+
+        
+
+        
+        
     }
 }
