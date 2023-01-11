4f0801918161817c78618a0b63e78f801c5aca19
Pablo-Penasco <pccraig@icloud.com>
2023-01-10 20:56:01 -0500

    added Sum, Count, and Max commands to perform math operations on the numbers in the list.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -11,19 +11,33 @@ class Program
         List<int> numbers = new List<int>();
 
         //Ask for user to input a number. Keep adding it to the "numbers" list until they enter 0.
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
         while (userInput != "0")
         {
             
-            Console.Write("Please add a number to the list. Enter 0 when you are done: ");
+            Console.Write("Enter number: ");
             userInput = Console.ReadLine();
             int userNumber = int.Parse(userInput);
 
-            numbers.Add(userNumber);
+            if (userNumber !=0)
+            {
+                numbers.Add(userNumber);
+            }
+            
         }
         
+        // Use Sum command to add numbers in the list.
+        float total = numbers.Sum();
+        Console.WriteLine($"The sum is: {total}");
         
+        //Get the count of items in the list and divice the total by that.
+        float count = numbers.Count();
+        float average = total / count;
+        Console.WriteLine($"The average is: {average}");
 
-        
+        //Find the largest number.
+        int max = numbers.Max();
+        Console.WriteLine($"The largest number is: {max}");
 
         
 
