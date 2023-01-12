294d9359ceb83cb0ac28e96ccf40415f1694e571
Pablo-Penasco <pccraig@icloud.com>
2023-01-11 19:39:04 -0500

    first 3 functions defined but having an error on the 3rd

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        //DisplayWelcome - Displays the message, "Welcome to the Program!" static function
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        
+        //Prompt User Name static function.
+        static string GetUserName(string userName)
+        {
+            Console.Write("Please enter your username: ");
+            userName = Console.ReadLine();
+
+            return userName;
+        }
+
+
+
+        //Call the funtions
+        DisplayWelcome();
+        GetUserName();
+
     }
+    
 }
