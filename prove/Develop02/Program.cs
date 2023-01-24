e7f1bbcb34183172b9d1cc1cf71d4237013f57d4
Pablo-Penasco <pccraig@icloud.com>
2023-01-24 18:09:24 -0500

    Creating classes and defining attributes and methods.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,5 +1,51 @@
 using System;
 
+//Create class called "Journal" with methods: journal entry, save file, load file. Attributes: list of entries
+public class Journal
+{
+    public string _journalEntry = "";
+
+    //Constructor for the new class
+    public Journal()
+    {
+
+    }
+
+    //Method #1 used to save the journal entry to a file
+    public void SaveFile()
+    {
+
+    }
+
+    //Method #2 used to load the entry in the file and make it available as a string to be displayed
+    public void LoadFile()
+    {
+
+    }
+}
+
+
+//Create a new Class called Prompt Generator to generate prompts for the user and display them.
+public class PrompGenerator
+{
+    //Constructor for the new class
+    public PrompGenerator()
+    {
+
+    }
+
+    //Method #1 Displays a prompt to the user from a list of prompts. Always chooses a fresh prompt.
+    public void DisplayPrompt()
+    {
+        //list of prompts to choose from
+
+        //how will the DisplayPrompt method choose between the different 
+    }
+
+
+
+}
+
 class Program
 {
     static void Main(string[] args)
